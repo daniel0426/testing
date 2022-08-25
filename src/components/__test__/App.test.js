1f911926e3c839d95ecd93df93e723bc10d6daaa
Daniel <angus9209@gmail.com>
2022-08-26 10:59:55 +1200

    component test with enzyme and changed import path configured

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,12 +1,18 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
 
 it('shows a comment box', ()=> {
-  const div = document.createElement('div');
+  const wrapped = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+})
 
-  expect(div.innerHTML).toContain('Comment Box')
-  ReactDOM.unmountComponentAtNode(div )
+it('shows a comment List', ()=> {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1)
 })
