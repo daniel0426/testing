8cb505ae5ba1270f6b6b15e4f7dd2bde72e5209b
Daniel <angus9209@gmail.com>
2022-08-29 12:49:15 +1200

    using redux-promise middleware for http request

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -3,26 +3,30 @@ import {connect} from 'react-redux';
 import * as actions from 'actions'
 
 class CommentBox extends Component {
-  state ={comment: ''};
-  
-  handleChange = (e) => {
-    this.setState({comment: e.target.value})
-  };
-  handleSubmit = (e)=> {
-    e.preventDefault();
-    this.props.saveComment(this.state.comment);
-    this.setState({comment: ''});
-  }
+  state = { comment: '' }
 
-  render(){
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.saveComment(this.state.comment)
+    this.setState({ comment: '' })
+  }
+  fetchComment = () => {}
+
+  render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add a Comment</h4>
-        <textarea onChange={this.handleChange} value={this.state.comment}/>
-        <div>
-          <button>Sumbit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Sumbit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fecth Comments</button>
+      </div>
     )
   }
 }
